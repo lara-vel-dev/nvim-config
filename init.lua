e2cbfd9c8f3efc9a79bbfa5c7d9338a662d0a05f
Daniela Lara <laradni@icloud.com>
2025-12-15 00:31:19 -0600

    (Add) lazy neovim

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
+-- plugin manager
+require "user.lazy"
+
 -- core directory files
 require "user.core.options"
 require "user.core.keymaps"
 require "user.core.colorscheme"
 
--- plugin manager
-require "user.lazy"
