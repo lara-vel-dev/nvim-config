239d62017a7becc620d6c1ede09a0f4de727e738
Daniela Lara <laradni@icloud.com>
2025-12-23 15:04:33 -0600

    (Add) main plugins

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -2,6 +2,7 @@ return {
 	"nvim-lua/plenary.nvim",
 	"christoomey/vim-tmux-navigator",
 	"barrett-ruth/live-server.nvim",
-	build = "pnpm add -g live-server",
+	build = "npm add -g live-server",
+	ft = { "html", "css", "scss", "javascript", "typescript" },
 	config = true,
 }
